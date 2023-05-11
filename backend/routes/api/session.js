c6210c1da907edd34c568ff607f096b69169eb5a
Edward <eghuffstetler@gmail.com>
2023-05-11 16:34:45 -0400

    Add User input validation on user signup backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -10,14 +10,14 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const validateLogin = [
     check('credential')
-      .exists({ checkFalsy: true })
-      .notEmpty()
-      .withMessage('Please provide a valid email or username.'),
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
     check('password')
-      .exists({ checkFalsy: true })
-      .withMessage('Please provide a password.'),
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
     handleValidationErrors
-  ];
+];
 
 const router = express.Router();
 
