d09fb63c6db4e5d945116354dde7ef250032bff7
Edward <eghuffstetler@gmail.com>
2023-05-14 11:19:16 -0400

    added options object for migrations for postgress

--- backend/db/migrations/20230513161753-unique-constraint-members.js
+++ backend/db/migrations/20230513161753-unique-constraint-members.js
@@ -1,18 +1,24 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addIndex('Members',
+    await queryInterface.addIndex(
+      'Members',
       ['groupId', 'userId'],
       {
         unique: true,
         name: 'idx_unique_Members_groupId_userId'
-      }
+      },
+      options
     );
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeIndex('Members', 'idx_unique_Members_groupId_userId');
+    await queryInterface.removeIndex('Members', 'idx_unique_Members_groupId_userId', options);
   }
 };
