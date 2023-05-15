3d82294cd11916c777db38bdc178e53602d85f12
Edward <eghuffstetler@gmail.com>
2023-05-15 15:49:34 -0400

    removed default value on GrouImages.preview to fix render error

--- backend/db/migrations/20230513163521-create-group-image.js
+++ backend/db/migrations/20230513163521-create-group-image.js
@@ -29,7 +29,7 @@ module.exports = {
       preview: {
         type: Sequelize.BOOLEAN,
         allowNull: false,
-        defaultValue: false
+        //defaultValue: false
       },
       createdAt: {
         allowNull: false,
