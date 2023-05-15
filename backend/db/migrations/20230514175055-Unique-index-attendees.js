d2380be9028d73da6b6a8ab23b71d0b60d1c5f97
Edward <eghuffstetler@gmail.com>
2023-05-15 14:14:49 -0400

    changed table name to singular for unique indexes

--- backend/db/migrations/20230514175055-Unique-index-attendees.js
+++ backend/db/migrations/20230514175055-Unique-index-attendees.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.addIndex(
-      'Attendees',
+      'Attendee',
       ['eventId', 'userId'],
       {
         unique: true,
