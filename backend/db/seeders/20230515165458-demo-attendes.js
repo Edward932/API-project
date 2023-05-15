8dbd3be0f2cd61fa3914a8220ee967b9e3211f43
Edward <eghuffstetler@gmail.com>
2023-05-15 13:37:36 -0400

    added seeder for venues;

---
+++ backend/db/seeders/20230515165458-demo-attendes.js
@@ -0,0 +1,58 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+const { Event, User, Attendee } = require('../models');
+
+const seed = [
+  {
+    event: 'new Event 2',
+    user: 'Demo-lition',
+    status: 'pending'
+  },
+  {
+    event: 'new Event 2',
+    user: 'FakeUser1',
+    status: 'pending'
+  },
+  {
+    event: 'Tennis Group First Meet and Greet',
+    user: 'FakeUser1',
+  },
+];
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    for(let i = 0; i < seed.length; i++) {
+      const event = await Event.findOne({
+        attributes: ['id'],
+        where: {
+          name: seed[i].event
+        }
+      });
+
+      const user = await User.findOne({
+        where: {
+          username: seed[i].user
+        }
+      });
+
+      const status = seed[i].status;
+
+      await Attendee.create({
+        eventId: event.id,
+        userId: user.id,
+        status
+      });
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Attendees'
+    await queryInterface.bulkDelete(options);
+  }
+};
