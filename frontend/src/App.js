aebe62c844bf19eaa7db17999c878444333a59ab
Edward <eghuffstetler@gmail.com>
2023-06-02 15:47:26 -0400

    created login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
-function App() {
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+
+export default function App() {
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
-
-export default App;
