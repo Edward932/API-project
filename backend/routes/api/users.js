944cb5b0a0e392c9d1b5aa20783326c618714293
Edward <eghuffstetler@gmail.com>
2023-06-06 15:33:54 -0400

    added signup errors

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,36 +1,36 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
+// const { check } = require('express-validator');
+// const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
-const validateSignup = [
-    check('email')
-        .exists({ checkFalsy: true })
-        .isEmail()
-        .withMessage('Please provide a valid email.'),
-    check('username')
-        .exists({ checkFalsy: true })
-        .isLength({ min: 4 })
-        .withMessage('Please provide a username with at least 4 characters.'),
-    check('username')
-        .not()
-        .isEmail()
-        .withMessage('Username cannot be an email.'),
-    check('password')
-        .exists({ checkFalsy: true })
-        .isLength({ min: 6 })
-        .withMessage('Password must be 6 characters or more.'),
-    handleValidationErrors
-];
+// const validateSignup = [
+//     check('email')
+//         .exists({ checkFalsy: true })
+//         .isEmail()
+//         .withMessage('Please provide a valid email.'),
+//     check('username')
+//         .exists({ checkFalsy: true })
+//         .isLength({ min: 4 })
+//         .withMessage('Please provide a username with at least 4 characters.'),
+//     check('username')
+//         .not()
+//         .isEmail()
+//         .withMessage('Username cannot be an email.'),
+//     check('password')
+//         .exists({ checkFalsy: true })
+//         .isLength({ min: 6 })
+//         .withMessage('Password must be 6 characters or more.'),
+//     handleValidationErrors
+// ];
 
 const router = express.Router();
 
 // Sign up
-router.post('/', validateSignup, async (req, res, next) => {
+router.post('/', async (req, res, next) => {
     const { email, password, username, firstName, lastName } = req.body;
     let hashedPassword;
     try{
