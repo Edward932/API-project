e43e5b72bf5b4034e40b74de6f54dff7dc45d755
Edward <eghuffstetler@gmail.com>
2023-06-06 18:47:56 -0400

    added basics of display event

---
+++ frontend/src/components/Events/EventPage/index.js
@@ -0,0 +1,115 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, Link } from "react-router-dom";
+import { getEventByIdThunk } from "../../../store/events";
+import './EventPage.css'
+import { getGroupByIdThunk } from "../../../store/groups";
+
+export default function EventPage() {
+    const { eventId } = useParams();
+    const dispatch = useDispatch();
+
+    const event = useSelector(state => state.events.singleEvent);
+    const group = useSelector(state => state.groups.singleGroup);
+
+    const previewImage = event.EventImages?.find(image => image.preview === true);
+    const previewUrl = previewImage?.url;
+    const groupPreviewImage = group.GroupImages?.find(image => image.preview === true);
+    const groupPreviewUrl = groupPreviewImage?.url;
+
+    const time = new Date(event.startDate);
+    const date = time.toDateString();
+    const timeOfDay = time.toTimeString().split(' ')[0];
+    const timeArr = timeOfDay.split(':');
+    if(timeArr[0] > 12) {
+        timeArr[0] -= 12;
+        timeArr[1] += ' PM'
+    } else {
+        timeArr[0] += ' AM'
+    }
+    timeArr.pop();
+    const normalizedFor12 = timeArr.join(':');
+
+    const timeEnd = new Date(event.endDate);
+    const dateEnd = timeEnd.toDateString();
+    const timeOfDayEnd = timeEnd.toTimeString().split(' ')[0];
+    const timeArrEnd = timeOfDayEnd.split(':');
+    if(timeArrEnd[0] > 12) {
+        timeArrEnd[0] -= 12;
+        timeArrEnd[1] += ' PM'
+    } else {
+        timeArrEnd[0] += ' AM'
+    }
+    timeArrEnd.pop();
+    const normalizedFor12End = timeArr.join(':');
+
+    useEffect(() => {
+        (async () => {
+            const currEvent = await dispatch(getEventByIdThunk(eventId));
+            const groupId = currEvent.Group?.id;
+            dispatch(getGroupByIdThunk(groupId));
+        })()
+
+    }, [dispatch, eventId]);
+
+    let hostButtons = null;
+    // if(user && user.id === group.Organizer?.id) {
+    //     hostButtons = (
+    //         <div>
+    //             <button>
+
+    //             </button>
+    //         </div>
+    //     )
+    // }
+
+
+    let eventPage = (<h3 className="loading">Loading ....</h3>);
+
+    if(event && +eventId === event?.id) {
+        eventPage = (
+            <div className="event-page-div">
+                <Link to="/events">{'< Events'}</Link>
+                <h1>{event.name}</h1>
+                <p>{group.Organizer?.firstName ? `Hosted by ${group.Organizer?.firstName} ${group?.Organizer?.lastName}` : 'No host registered for group'}</p>
+                <div id="event-main-div">
+                    {previewImage ? <img src={previewUrl} id="event-preview-img" alt="preview"/> : <p id="event-preview-img">No preview image available</p>}
+                    <div>
+                        <div id='event-group-display'>
+                            {groupPreviewUrl ? <img src={groupPreviewUrl} id="event-group-small" alt="group"/> : <p id="event-group-small">No preview image available</p>}
+                            <div>
+                                <h4>{event.Group.name}</h4>
+                                <p>{event.Group.private ? 'Private' : 'Public'}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div id="event-time">
+                               <i className="fa-regular fa-clock"></i>
+                                <div id='event-start-end'>
+                                    <p className="event-times">START: {date} <i className="fa-solid fa-circle"></i> {normalizedFor12}</p>
+                                    <p className="event-times">END:  {dateEnd} <i className="fa-solid fa-circle"></i> {normalizedFor12End}</p>
+                                </div>
+                            </div>
+                            <div className="event-money-type">
+                                <i className="fa-solid fa-dollar-sign"></i>
+                                <p>{event.price > 0 ? `PRICE: $${event.price}` : 'FREE'}</p>
+                            </div>
+                            <div className="event-money-type">
+                                <i className="fa-solid fa-location-dot"></i>
+                                <p>{event.type}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <h3>Details</h3>
+                <p>{event.description}</p>
+            </div>
+        )
+    };
+
+    return (
+        <>
+            {eventPage}
+        </>
+    )
+}
--- frontend/src/store/events.js
+++ frontend/src/store/events.js
@@ -2,6 +2,7 @@
 
 const GET_EVENTS_BY_GROUP = 'events/getEventsByGroup';
 const GET_EVENTS = 'events/getEvents';
+const GET_EVENT_BY_ID = 'events/getEventById';
 
 const getEventsByGroup = events => {
     return {
@@ -14,6 +15,13 @@ const getEvents = (events) => {
     return {
         type: GET_EVENTS,
         events
+    }
+};
+
+const getEventById = (event) => {
+    return {
+        type: GET_EVENT_BY_ID,
+        event
     }
 };
 
@@ -41,6 +49,19 @@ export const getEventsThunk = () => async dispatch => {
         const error = await res.json();
         return error;
     }
+};
+
+export const getEventByIdThunk = eventId => async dispatch => {
+    const res = await fetch(`/api/events/${eventId}`);
+
+    if(res.ok) {
+        const event = await res.json();
+        dispatch(getEventById(event));
+        return event;
+    } else {
+        const error = await res.json();
+        return error;
+    }
 }
 
 const initialState = { allEvents: {}, singleEvent: {}, groupEvents: [] };
@@ -50,13 +71,14 @@ const eventReducer = (state = initialState, action) => {
         case GET_EVENTS_BY_GROUP:
             return { ...state, groupEvents: action.events.Events };
         case GET_EVENTS:
-            console.log("IN REDUCER")
             const normalizedEvents = {};
             const eventArr = action.events.Events;
             eventArr.forEach(event => {
                 normalizedEvents[event.id] = event;
             });
-            return { ...state, allEvents: normalizedEvents }
+            return { ...state, allEvents: normalizedEvents };
+        case GET_EVENT_BY_ID:
+            return { ...state, singleEvent: action.event}
         default:
             return state;
     }
